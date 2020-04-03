a4624275bbdca2531d05995f6e3a9b30db3cd2ee
Andrew Gardner <agardner@securenettech.com>
2020-04-03 14:30:58 -0600

    Updates.

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,176 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-content>
+      <div class=gvt>
+        <div class=gvt style='max-width:800px;'>
+          <v-card style='width:100%;height:95%;'>
+            <div class=gvt style='--gr:1fr;--gc:3fr 1fr;'>
+              <div class=gvt style='--gr:1fr;--gc:1fr;position:relative;'>
+                <div style='background:white;position:absolute;height:100%;width:100%;overflow:auto;'>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+                  <div class=gvt style='height:auto;width:calc(100% - 24px);--gr:1fr;--gc:56px auto 1fr;margin:32px 12px;'>
+                    <v-icon size=36 color="#35869A">mdi-read</v-icon>
+                    <span style='color:#35869A;font-size:24px;font-weight:bold;'>Tl;DR</span>
+                  </div>
+
+                  <div class=gvt style='height:auto;--gr:auto auto;--gc:auto 1fr auto;'>
+                    <span style='place-self:start;margin-left:24px;font-size:16px;font-weight:bold;'>Engineering Diary</span>
+                    <span></span>
+                    <span style='color:gray;margin-right:24px;font-size:12px;'>2 Years +</span>
+                    <span style='place-self:start;color:gray;margin-left:24px;font-size:12px;'>Over 70 Videos and growing</span>
+                  </div>
+
+                  <div style='margin:26px;font-size:12px;color:black;overflow-wrap: break-word;'>
+                    Easiest way to get to know me is on youtube, pick a playlist, set playback to 2x!<br><br>
+                    <span><a href='https://www.youtube.com/channel/UC4kH4isGJvL2vWhH5JOWyVw/playlists' target='_blank'>Playlists</a></span><br>
+                    <span><a href='https://www.youtube.com/watch?v=NP1FFL8TeUA&list=PLFzhufYbYGVgkBGDaaaGQM9f5lrHRzDSe' target='_blank'>Project Showcases</a></span><br>
+                    <span><a href='https://www.youtube.com/watch?v=dfv1z8CHMwY&list=PLFzhufYbYGVioRvOdld-O6a3BHWVCTz8c' target='_blank'>Programming Concepts</a></span><br>
+                    <span><a href='https://www.youtube.com/watch?v=f2eAwpjB1Sc&list=PLFzhufYbYGVg19ids9l9A-V4pAi4KPLbd' target='_blank'>Architecture</a></span><br>
+                    <span><a href='https://www.youtube.com/watch?v=vxrxxvRw2RM&list=PLFzhufYbYGVhRIZK0KBircBQEtfXMJ5q4' target='_blank'>Research</a></span><br>
+                  </div>
+
+                  <div class=gvt style='height:auto;width:calc(100% - 24px);--gr:1fr;--gc:56px auto 1fr;margin:32px 12px;'>
+                    <v-icon size=36 color="#35869A">mdi-tools</v-icon>
+                    <span style='color:#35869A;font-size:24px;font-weight:bold;'>Skills</span>
+                  </div>
+
+                  <div class=gvt style='height:auto;--gr:auto auto;--gc:auto 1fr auto;'>
+                    <span style='place-self:start;margin-left:24px;font-size:16px;font-weight:bold;'>Statically Typed Object Oriented Languages</span>
+                    <span></span>
+                    <span style='color:gray;margin-right:24px;font-size:12px;'>10 Years +</span>
+                    <span style='place-self:start;color:gray;margin-left:24px;font-size:12px;'>C++, C#, Java</span>
+                  </div>
+
+                  <div style='margin:26px;font-size:12px;color:black;overflow-wrap: break-word;'>
+                    Classes, Inheritance, Interfaces, Generics / Template Programming, Stack vs Heap, Memory Management
+                    <span style='font-size:18px;font-weight:bold;'>...</span>
+                  </div>
+
+                  <div class=gvt style='height:auto;--gr:auto auto;--gc:auto 1fr auto;'>
+                    <span style='place-self:start;margin-left:24px;font-size:16px;font-weight:bold;'>Front End / Web Develompent</span>
+                    <span></span>
+                    <span style='color:gray;margin-right:24px;font-size:12px;'>5 Years +</span>
+                    <span style='place-self:start;color:gray;margin-left:24px;font-size:12px;'>HTTP, HTML, CSS, Javascript | Typescript</span>
+                  </div>
+
+                  <div style='margin:26px;font-size:12px;color:black;overflow-wrap: break-word;'>
+                    VueJS, VueX, VueRouter, Components, Data Binding, Global Program States, Progressive Web Applications, 
+                    Manifest, Open Graph, Twitter Cards, Service Worker, Offline Mode, Caching, Concurrency ( Worker Threads ), WebAssembly, 
+                    Emscripten, Async, Requests, Fetch, GET, POST, PUT, DELETE, NPM, Javascript Modules <span style='font-size:18px;font-weight:bold;'>...</span>
+                  </div>
+
+                  <div class=gvt style='height:auto;--gr:auto auto;--gc:auto 1fr auto;'>
+                    <span style='place-self:start;margin-left:24px;font-size:16px;font-weight:bold;'>Back End / Web Develompent</span>
+                    <span></span>
+                    <span style='color:gray;margin-right:24px;font-size:12px;'>5 Years +</span>
+                    <span style='place-self:start;color:gray;margin-left:24px;font-size:12px;'>NodeJS, Express ..., MongoDB, Python, Django</span>
+                  </div>
+
+                  <div style='margin:26px;font-size:12px;color:black;overflow-wrap: break-word;'>
+                    Docker, Load Balancing, Microservices, AWS
+                  </div>
+
+                  <div class=gvt style='height:auto;width:calc(100% - 24px);--gr:1fr;--gc:56px auto 1fr;margin:32px 12px;'>
+                    <v-icon size=36 color="#35869A">mdi-account-circle</v-icon>
+                    <span style='color:#35869A;font-size:24px;font-weight:bold;'>Career</span>
+                  </div>
+
+                  <div class=gvt style='height:auto;width:calc(100% - 24px);--gr:1fr;--gc:56px auto 1fr;margin:32px 12px;'>
+                    <v-icon size=36 color="#35869A">mdi-compass</v-icon>
+                    <span style='color:#35869A;font-size:24px;font-weight:bold;'>Philosophy</span>
+                  </div>
+
+                  <div class=gvt style='height:auto;width:calc(100% - 24px);--gr:1fr;--gc:56px auto 1fr;margin:32px 12px;'>
+                    <v-icon size=36 color="#35869A">mdi-history</v-icon>
+                    <span style='color:#35869A;font-size:24px;font-weight:bold;'>History</span>
+                  </div>
+
+                </div>
+              </div>
+              <div class=gvt style='--gr:1fr 3fr;--gc:1fr;'>
+                <div class=gvt style='background:#35869A;--gr:3fr 1fr 1fr;--gc:1fr;'>
+                  <div class=gvt style='--gr:1fr;--gc:1fr;background:white;height:100px;width:100px;border-radius:50%;'>
+                    <span style='color:#35869A;font-size:48px;font-weight:bold;'>AG</span>
+                  </div>
+                  <div style='font-weight:bold;color:white;font-size:24px;'>Andrew Gardner</div>
+                  <div style='color:white;'>Software Engineer</div>
+                </div>
+                <div class=gvt style='background:#42A9C1;--gr:1fr;--gc:1fr;place-items: start;'>
+                  <v-list-item style='width:100%;'>
+                    <v-list-item-content style='width:100%;'>
+                      <v-list-item-title style='width:100%;color:white;'>
+                        <div style='width:100%;text-align:left;font-weight:bold;color:white;margin-bottom:8px;'>
+                          Contact
+                        </div>
+                        <div class=gvt style='--gr:1fr;--gc: 40px auto 1fr;margin-bottom:16px;margin-top:32px;'>
+                          <v-icon color=white>mdi-email</v-icon> 
+                          <span style='font-size:14px;'>agardnerut@gmail.com</span>
+                        </div>
+                        <div class=gvt style='--gr:1fr;--gc: 40px auto 1fr;margin-bottom:16px;'>
+                          <v-icon color=white>mdi-phone-ring</v-icon> 
+                          <span style='font-size:14px;'>801-245-9947</span>
+                        </div>
+                        <div class=gvt style='--gr:1fr;--gc: 40px auto 1fr;margin-bottom:16px;'>
+                          <v-icon color=white>mdi-link</v-icon> 
+                          <span><a style='color:white;' href='https://andrew.d8data.io' target='_blank'>andrew.d8data.io</a></span>
+                        </div>
+                        <div class=gvt style='--gr:1fr;--gc: 40px auto 1fr;margin-bottom:16px;'>
+                          <v-icon color=white>mdi-git</v-icon> 
+                          <span><a style='color:white;' href='https://github.com/justdoityourself' target='_blank'>https://github.com</a></span>
+                        </div>
+                        <div class=gvt style='--gr:1fr;--gc: 40px auto 1fr;margin-bottom:16px;'>
+                          <v-icon color=white>mdi-youtube</v-icon> 
+                          <span><a style='color:white;' href='https://www.youtube.com/channel/UC4kH4isGJvL2vWhH5JOWyVw' target='_blank'>https://youtube.com</a></span>
+                        </div>
+                        <div class=gvt style='--gr:1fr;--gc: 40px auto 1fr;margin-bottom:48px;'>
+                          <v-icon color=white>mdi-discord</v-icon> 
+                          <span><a style='color:white;' href='https://discord.gg/9DCcj2D' target='_blank'>https://discord.com</a></span>
+                        </div>
+
+                        <div style='width:100%;text-align:left;font-weight:bold;color:white;margin-bottom:8px;'>
+                          Innovator
+                        </div>
+                        <div class=gvt style='--gr:1fr;--gc: 40px auto 1fr;margin-bottom:16px;'>
+                          <v-icon color=white>mdi-file</v-icon> 
+                          <span><a style='color:white;' href='https://www.google.com/search?tbo=p&tbm=pts&hl=en&q=ininventor:Andrew+ininventor:Lynn+ininventor:Gardner&num=10' target='_blank'>Patents</a></span>
+                        </div>
+                        <div class=gvt style='--gr:1fr;--gc: 40px auto 1fr;margin-bottom:16px;'>
+                          <v-icon color=white>mdi-graph</v-icon> 
+                          <span><a style='color:white;' href='https://math.d8data.io' target='_blank'>Research Site</a></span>
+                        </div>
+
+                        <div style='width:100%;text-align:left;font-weight:bold;color:white;margin-bottom:8px;'>
+                          Mentor
+                        </div>
+                        <div class=gvt style='--gr:1fr;--gc: 40px auto 1fr;margin-bottom:16px;'>
+                          <v-icon color=white>mdi-timelapse</v-icon> 
+                          <span><a style='color:white;' href='https://discord.gg/9DCcj2D' target='_blank'>Regular Classes</a></span>
+                        </div>
+                        <div class=gvt style='--gr:1fr;--gc: 40px auto 1fr;margin-bottom:16px;'>
+                          <v-icon color=white>mdi-video</v-icon> 
+                          <span><a style='color:white;' href='https://www.youtube.com/channel/UC4kH4isGJvL2vWhH5JOWyVw' target='_blank'>Engineering Diary</a></span>
+                        </div>
+                        <div style='width:100%;text-align:left;font-weight:bold;color:white;margin-bottom:8px;'>
+                          Contributor
+                        </div>
+                        <div class=gvt style='--gr:1fr;--gc: 40px auto 1fr;margin-bottom:16px;'>
+                          <v-icon color=white>mdi-open-source-initiative</v-icon> 
+                          <span><a style='color:white;' href='https://github.com/justdoityourself' target='_blank'>Open Source</a></span>
+                        </div>
+                        <div class=gvt style='--gr:1fr;--gc: 40px auto 1fr;margin-bottom:16px;'>
+                          <v-icon color=white>mdi-alarm-check</v-icon> 
+                          <span><a style='color:white;' href='https://www.youtube.com/watch?v=NP1FFL8TeUA&list=PLFzhufYbYGVgkBGDaaaGQM9f5lrHRzDSe' target='_blank'>Completion</a></span>
+                        </div>
+                      </v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </div>
+              </div>
+            </div>
+          </v-card>
+        </div>
       </div>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
     </v-content>
   </v-app>
 </template>
@@ -56,3 +188,24 @@ export default {
   }),
 };
 </script>
+
+<style>
+  @import './assets/vgrid.css';
+
+    ::-webkit-scrollbar {
+        width: 2px;
+        height:10px;
+    }
+
+    ::-webkit-scrollbar-track {
+        background: var(--scroll-track, #f1f1f1);
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background: var(--scroll-thumb, #888);
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        background: var(--scroll-thumb-hover, #555);
+    }
+</style>
